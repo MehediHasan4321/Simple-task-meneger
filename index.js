255666f4db4da73d140bd28fc0fdf953d7c2c1cd
MehediHasan4321 <126mehedi@gmail.com>
2023-02-09 13:56:14 +0600

    add color palet

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ inputField.addEventListener("keypress", function (e) {
 
     if (e.keyCode === 13) {
         let value = e.target.value;
+        
         createTask(taskContainer, value)
         e.target.value = ""
     }
@@ -21,20 +22,51 @@ inputField.addEventListener("keypress", function (e) {
 
 function createTask(parent, mgs) {
     let div = document.createElement("div");
-    let i = document.createElement("div")
+    let span = document.createElement("span");
     let p = document.createElement("p");
     div.appendChild(p)
     p.innerText = mgs
-    i.innerHTML = `<i class="fa-solid fa-trash-can"></i>`
-    i.classList = "icon"
+    span.innerHTML = `<i class="fa-solid fa-trash-can"></i>`
+    //span.style="ml-auto"
+    //i.classList = "icon"
 
-    i.addEventListener("click", function () {
+    span.addEventListener("click", function () {
         parent.removeChild(div)
     })
-    div.appendChild(i)
+    div.appendChild(span)
+    let taskControler = createTaskControlar(div);
+    div.onmouseenter=function(){
+        taskControler.style.visibility ="visible"
+    };
+    div.onmouseleave=()=>{
+        taskControler.style.visibility="hidden"
+    }
+   div.appendChild(taskControler)
     div.classList.add("task-box")
     parent.appendChild(div)
 
+}
 
+function createTaskControlar(parent){
+    let div = document.createElement("div");
+    div.classList.add("task-controler-div")
+    parent.appendChild(div)
+    let colorPalet = createColorPalet();
+    div.appendChild(colorPalet)
     return div
+}
+
+function createColorPalet(){
+    let colors = ["red","#D88771","rgb(255,0,255)","green","yellow"];
+
+    let colorDiv = document.createElement("div");
+     colorDiv.classList.add("color-box");
+    colors.forEach(color=>{
+        let div = document.createElement("div");
+        div.style.backgroundColor = color
+        div.classList.add("color-div")
+        colorDiv.appendChild(div)
+    })
+
+    return colorDiv;
 }
